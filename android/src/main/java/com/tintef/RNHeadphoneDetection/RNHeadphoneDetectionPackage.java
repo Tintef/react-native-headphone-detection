71a493246f23c41f1af0dcb345b56525faca82f6
Igor Kuznietsov <igor.kuznietsov@expercast.com>
2021-12-06 17:09:56 +0200

    rollback changes

--- android/src/main/java/com/tintef/RNHeadphoneDetection/RNHeadphoneDetectionPackage.java
+++ android/src/main/java/com/tintef/RNHeadphoneDetection/RNHeadphoneDetectionPackage.java
@@ -1,5 +1,5 @@
 
-package com.tintef.RNHeadphoneDetection;
+package com.tintef.HeadphoneDetection;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -10,7 +10,6 @@ import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 import com.facebook.react.bridge.JavaScriptModule;
-
 public class RNHeadphoneDetectionPackage implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
